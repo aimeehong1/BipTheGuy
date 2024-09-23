6c64f7e173bf26587ecf5749b4846ea52cef3f6b
Aimee Hong <honglp@bc.edu>
2024-09-23 15:25:30 -0400

    finished bip transition, noise, and photo library option

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -6,16 +6,67 @@
 //
 
 import SwiftUI
+import AVFAudio
+import PhotosUI
 
 struct ContentView: View {
+    @State private var audioPlayer: AVAudioPlayer!
+    @State private var animateImage = true
+    @State private var selectedPhoto: PhotosPickerItem?
+    @State private var bipImage = Image("clown")
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            bipImage
+                .resizable()
+                .scaledToFit()
+                .scaleEffect(animateImage ? 1.0 : 0.9)
+                .onTapGesture {
+                    playSound(soundName: "punchSound")
+                    animateImage = false // will immediately shrink using .scaleEffect to 90% of size
+                    withAnimation(.spring(response: 0.3, dampingFraction: 0.4)) {
+                        animateImage = true // will go from 90% size to 100% using the .spring animation
+                    }
+                }
+            
+            Spacer()
+            
+            PhotosPicker(selection: $selectedPhoto, matching: .images, preferredItemEncoding: .automatic) {
+                Label("Photo Library", systemImage: "photo.fill.on.rectangle.fill")
+            }
+            .onChange(of: selectedPhoto) {
+                Task {
+                    do {
+                        if let data = try await selectedPhoto?.loadTransferable(type: Data.self) {
+                            if let uiImage = UIImage(data: data) {
+                                bipImage = Image(uiImage: uiImage)
+                            }
+                        }
+                    } catch {
+                        print("😡 ERROR: loading failed \(error.localizedDescription)")
+                    }
+                }
+            }
         }
         .padding()
+    }
+    
+    func playSound(soundName: String) {
+        if audioPlayer != nil {
+            audioPlayer.stop()
+        }
+        
+        guard let soundFile = NSDataAsset(name: soundName) else {
+            print("😡 Could not read file named \(soundName)")
+            return
+        }
+        do {
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.play()
+        } catch {
+            print("😡 ERROR: \(error.localizedDescription) creating audioplayer.")
+        }
     }
 }
 
