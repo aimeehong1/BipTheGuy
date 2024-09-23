3ef3e7af07a36284354e30eef6b525eabdb993ef
Aimee Hong <honglp@bc.edu>
2024-09-23 11:43:13 -0400

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by Aimee Hong on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BipTheGuyTests/BipTheGuyTests.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyTests.swift
+//  BipTheGuyTests
+//
+//  Created by Aimee Hong on 9/23/24.
+//
+
+import Testing
+@testable import BipTheGuy
+
+struct BipTheGuyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
